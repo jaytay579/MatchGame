817cd50c09bfa0d5c5388a05ea42226308b1c879
jaytay579 <jasontaylor579@gmail.com>
2024-01-23 02:43:03 +0000

    resetting for class demo!

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -15,37 +15,15 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
-    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer = new DispatcherTimer();
-        int tenthsOfSecondsElapsed;
-        int matchesFound;
-
         public MainWindow()
         {
             InitializeComponent();
-            timer.Interval = TimeSpan.FromSeconds(0.1);
-            timer.Tick += Timer_Tick;
             SetUpGame();
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            tenthsOfSecondsElapsed++;
-            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
-            if (matchesFound == 8 || tenthsOfSecondsElapsed > 100)
-            {
-                timer.Stop();
-                if (tenthsOfSecondsElapsed > 100)
-                {
-                    alarmClock.Visibility = Visibility.Visible;
-                }
-                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
-            }
         }
 
         private void SetUpGame()
@@ -66,53 +44,12 @@ namespace MatchGame
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                if (textBlock.Name != "timeTextBlock" && textBlock.Name !="alarmClock")
-                {
-                    textBlock.Visibility = Visibility.Visible;
-                    int index = random.Next(animalEmoji.Count);
-                    string nextEmoji = animalEmoji[index];
-                    textBlock.Text = nextEmoji;
-                    animalEmoji.RemoveAt(index);
-                }
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
             }
 
-            alarmClock.Visibility = Visibility.Hidden;
-            timer.Start();
-            tenthsOfSecondsElapsed = 0;
-            matchesFound = 0;
-        }
-
-        TextBlock lastTextBlockClicked;
-        bool findingMatch = false;
-
-        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            TextBlock textBlock = sender as TextBlock;
-            if (findingMatch == false)
-            {
-                textBlock.Visibility = Visibility.Hidden;
-                lastTextBlockClicked = textBlock;
-                findingMatch = true;
-            }
-            else if (textBlock.Text == lastTextBlockClicked.Text)
-            {
-                matchesFound++;
-                textBlock.Visibility = Visibility.Hidden;
-                findingMatch = false;
-            }
-            else
-            {
-                lastTextBlockClicked.Visibility = Visibility.Visible;
-                findingMatch = false;
-            }
-        }
-
-        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (matchesFound == 8 || tenthsOfSecondsElapsed > 100)
-            {
-                SetUpGame();
-            }
         }
     }
 }
